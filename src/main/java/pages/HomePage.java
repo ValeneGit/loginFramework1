ff24ad26b310cae97fce8bdce3f0645cf723443b
ValeneGit <115575743+ValeneGit@users.noreply.github.com>
2022-10-18 17:39:14 +0200

    Add files via upload
    Automation Project

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private WebDriver driver;
+    private By formAuthenticationLink = By.linkText("Form Authentication");
+    //constructor
+    public HomePage (WebDriver driver){
+        this.driver = driver;
+
+    }
+
+    //clicking on the form authentication link
+    public LoginPage clickFormAuthenticationLink(){
+        driver.findElement(formAuthenticationLink).click();
+        return new LoginPage(driver);
+    }
+
+    public void clickLinkText(String linktext){
+        driver.findElement(By.linkText(linktext)).click();
+    }
+
+    public  ForgotPasswordPage clickForgotPwd(String linkText){
+        clickLinkText(linkText);
+        return new ForgotPasswordPage(driver);
+    }
+}
---
+++ src/test/java/baseTest/ForgotPwdTest.java
@@ -0,0 +1,32 @@
+package baseTest;
+
+import org.junit.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.ForgotPasswordPage;
+import pages.SecureAreaPage;
+
+public class ForgotPwdTest extends BaseTests {
+
+    @Test(priority = 2)
+    public void forgotPwdTest(){
+        ForgotPasswordPage forgotPasswordPage = homePage.clickForgotPwd("Forgot Password");
+        forgotPasswordPage.enterEmail("Just@do.it");
+        forgotPasswordPage.clickRetrievePwdButton();
+        Assert.assertEquals(forgotPasswordPage.getErrorMsg(),"Internal Server Error");
+    }
+
+
+    @DataProvider()
+    public Object[][] ForgotPasswordData(){
+        Object[][] data = new Object[4][3];
+
+        data[0][0] = "test@gmail.com"; data[0][1] = "Inspired@testing.com"; data[0][2] = true;
+        data[1][0] = "Automate@123.com"; data[1][1] = "Test@password.com"; data[1][2] = true;
+        data[2][0] = "Automate@123.com**"; data[2][1] = "Test@password.com**"; data[2][2] = false;
+        data[3][0] = "Automate@123.com()"; data[3][1] = "Test@password.com()"; data[3][2] = false;
+
+        return data;
+    }
+
+}
